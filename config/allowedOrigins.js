69dcb27dcd354235abe0229ff1908a6f5f79f29f
Zelalem <mekonnen.zelalem251@gmail.com>
2023-03-11 04:19:32 +0300

    Fix error in booking controller

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 export default [
-	'https://lambent-maamoul-6d6af3.netlify.app',
-	'http://localhost:3000',
-	'http://localhost:3001',
-	'http://localhost:3002',
-	undefined,
+  'https://lambent-maamoul-6d6af3.netlify.app',
+  'https://quiet-eclair-9af6cb.netlify.app',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+  undefined,
 ];
