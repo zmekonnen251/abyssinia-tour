55c2c3d6b882f6069d37f7c17ead9754aa01af0e
Zelalem <mekonnen.zelalem251@gmail.com>
2023-03-14 00:54:20 +0300

    fix allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 export default [
   'https://abyssinia-tour-client.vercel.app/',
+  'https://abyssinia-tour.onrender.com',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
