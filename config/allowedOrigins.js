35ea762806e3f981cd9634642c751791731d9b3a
Zelalem <mekonnen.zelalem251@gmail.com>
2023-03-14 02:08:50 +0300

    modified allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 export default [
   'https://abyssinia-tour-client.vercel.app',
   'https://abyssinia-tour.onrender.com',
-  'https://abyssinia-tour.netlify.app/'
+  'https://abyssinia-tour.netlify.app',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
